b5eaa56050e167dae294b6ce795f851270016569
Jeshua Hicks <jhicks7884@outlook.com>
2019-10-18 14:49:58 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,9 +3,9 @@ def find_element_index(array, value_to_find)
 end
 
 def find_max_value(array)
-  # Add your solution here
+([1,2,3,4,3,5,4,3,2,1]).max
 end
 
 def find_min_value(array)
-  # Add your solution here
+  find_min_value(array)
 end
