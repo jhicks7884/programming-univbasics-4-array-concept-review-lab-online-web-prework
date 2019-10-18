47cbb5b641c329fd0c968ee5a56fc8edeb060a9d
Jeshua Hicks <jhicks7884@outlook.com>
2019-10-18 14:51:58 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,7 +3,8 @@ def find_element_index(array, value_to_find)
 end
 
 def find_max_value(array)
-([1,2,3,4,3,5,4,3,2,1]).max
+array.max_by do |element|
+  element.field
 end
 
 def find_min_value(array)
